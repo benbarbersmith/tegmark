3bf7a6b0f4422cc4d77e48dbbde4d252616c469e
willwybrow <will@willwybrow.com>
2016-02-14 01:24:55 +0000

    choose land spikes based on seed

--- worldserver/generation.py
+++ worldserver/generation.py
@@ -7,7 +7,8 @@ from logger import logger
 
 
 def add_world_geography(world_id, global_state_dict, global_lock):
-    w = World(num_voronoi_cells=1500, seed=int(world_id, 16))
+    numeric_seed = int(world_id, 16)
+    w = World(num_voronoi_cells=2500, seed=numeric_seed, num_altitude_spikes=(numeric_seed&0xF/2)+2)
     g = geography.geography_from_everett_world(w)
     # for a in range(30):
     #     logger.debug(u"Waiting for {} seconds...".format(a))
