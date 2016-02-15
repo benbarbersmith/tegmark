c7fb27f1fbcae39b7a787df99e9ed151e22edd07
Benjamin Smith <web@benjaminasmith.com>
2016-02-15 22:52:59 +0000

    WIP canvas implementation for improved performance.

--- www/js/directives.js
+++ www/js/directives.js
@@ -2,7 +2,7 @@
 
 var tegmarkDirectives = angular.module('tegmarkDirectives', ['tegmarkServices']);
 
-tegmarkDirectives.directive('map', ['$interval', '$window', 'd3', 'World', function($interval, $window, d3, World) {
+tegmarkDirectives.directive('svgmap', ['$interval', '$window', 'd3', 'World', function($interval, $window, d3, World) {
   return {
     restrict: 'E',
     scope: {
@@ -226,3 +226,155 @@ tegmarkDirectives.directive('map', ['$interval', '$window', 'd3', 'World', funct
       }
     }
   }]);
+
+tegmarkDirectives.directive('map', ['$interval', '$window', 'd3', 'World', function($interval, $window, d3, World) {
+  return {
+    restrict: 'E',
+    scope: {
+      world: "=",
+      status: "="
+    },
+    link: function(scope, elements, attrs) {
+
+      var canvas = d3.select(elements[0])
+        .append("canvas")
+        .attr("width", "1px")
+        .attr("height", "1px")
+
+      var width = 1,
+          height = 1,
+          scale = 0.9 * 360,
+          projection = d3.geo.orthographic()
+            .scale(scale)
+            .clipAngle(90)
+            .translate([(width)/2, (height-90)/2]);
+
+      var path, poll, translate, visibleArea, invisibleArea;
+      var context = canvas.node().getContext("2d");
+      path = d3.geo.path().projection(projection);
+
+      var resize = function() {
+        if(elements[0].offsetParent == null) return null;
+        width = elements[0].offsetParent.offsetWidth - 2 * elements[0].offsetLeft;
+        height = elements[0].offsetParent.offsetHeight - 1.5 * elements[0].offsetTop;
+        canvas[0][0].width = width;
+        canvas[0][0].height = height;
+        canvas[0][0].style = "border: rgb(231, 231, 231) 1px solid;";
+        scale = 0.9 * 360;
+        projection = d3.geo.orthographic()
+          .scale(scale)
+          .clipAngle(90)
+          .translate([(width)/2, (height-90)/2]);
+        path = d3.geo.path().projection(projection);
+      }
+
+      var renderMap = function() {
+        context.clearRect(0, 0, width, height);
+        console.log("Rendering map.");
+
+        var globe = {type: "Sphere"};
+
+        context.strokeStyle = '#777';
+        context.fillStyle = '#fff';
+        context.beginPath();
+        path.context(context)(globe);
+        context.fill();
+        context.stroke();
+
+        var cells = topojson.feature(scope.world.topography, scope.world.topography.objects.land).features;
+
+        cells.forEach(function(d, i) {
+          context.fillStyle = getColorByAltitude(d, 0.7);
+          context.beginPath();
+          path.context(context)(d);
+          context.fill();
+          context.stroke();
+        });
+
+        console.log("Finished rendering map.");
+      }
+
+      var renderText = function() {
+        context.fillStyle = "#777";
+        context.font = "18px Helvetica Neue,Helvetica,Arial,sans-serif";
+        context.textAlign = "center";
+        context.textBaseline = "hanging";
+        context.fillText("Generating world...", Math.round(width/2), Math.round(height/2));
+
+        if(scope.status == "generating") {
+          poll = $interval(World.poll, 500);
+        } else {
+          if(typeof poll !== 'undefined') $interval.cancel(poll);
+        }
+      }
+
+      var render = function() {
+        resize();
+        if(typeof scope.world !== 'undefined' && scope.status == "complete") {
+          if(typeof poll !== 'undefined') $interval.cancel(poll);
+          renderMap();
+        }
+        else {
+          renderText();
+        }
+      }
+
+      var rgb = function(r, g, b) {
+        return [r,g,b];
+      }
+
+      var colourMix = function(bounds, distance, direction) {
+        return [0,1,2].map(function(i) {
+          var range = bounds[1][i] - bounds[0][i];
+          var direction = (bounds[0][i] < bounds[1][i]) ? 1 : -1;
+          return Math.round(bounds[0][i] + range*distance*direction);
+        });
+      }
+
+      var colourMap = {
+    		'permafrost' : [rgb(250,250,250), rgb(255,255,255)],
+    		'sea' : [rgb(57,139,207), rgb(0,62,130)],
+    		'lowlands' : [rgb(100,189,41), rgb(81,161,35)],
+    		'highlands' : [rgb(81,161,35), rgb(25,76,17)],
+    		'vegetation' : [rgb(100,189,41), rgb(25,76,17)],
+    		'alpine' : [rgb(150,109,33), rgb(255,255,255)]
+  	  }
+
+      var altitudeMapByLatitude = function(latitude) {
+        var highland_max = (-0.8*latitude*latitude + 3800) / 10000;
+        highland_max = highland_max < 0 ? 0.000001 : highland_max;
+        return {
+      		'sea' : [-1, 0],
+			    'permafrost' : [-1, 0],
+      		'vegetation' : [0, highland_max],
+          'lowlands' : [0, 0.05],
+          'highlands' : [0.05, highland_max],
+      		'alpine' : [highland_max, 1]
+        };
+  	  }
+
+      var getColorByAltitude = function(d, a) {
+        var latitude = d.geometry.coordinates[0].reduce(function(acc, i) {
+          acc += i[1];
+          return acc;
+        }, 0) / d.geometry.coordinates[0].length;
+        var terrain = d.properties.terrain_type;
+    		var altitudeBounds = altitudeMapByLatitude(latitude)[terrain];
+        var colourBounds = colourMap[terrain];
+        var distance = (d.properties.altitude - altitudeBounds[0]) / (altitudeBounds[1] - altitudeBounds[0]);
+        var rgb = colourMix(colourBounds, distance);
+        return "rgba("+rgb[0]+","+rgb[1]+","+rgb[2]+","+a+")";
+      }
+    
+      scope.$watch("world", function (newVal, oldVal) {
+        if (typeof newVal !== 'undefined' && newVal != oldVal) {
+          render()
+        }
+      });
+
+      angular.element($window).bind('resize', function(){
+        render();
+      });
+    }
+  }
+}]);
