f70844ffae490555ed300c3e1b360f337f00357b
Sam Corcoran <samuel.corcoran@gmail.com>
2016-09-27 21:29:26 +0100

    Brought tegmark in line with dev branch changes before they are merged into master

--- worldserver/generation.py
+++ worldserver/generation.py
@@ -1,15 +1,10 @@
-
-import time
 import geography
-from everett.world import World
-
-from logger import logger
+from everett.worldgenerators import world_one
 
 
 def add_world_topography(world_id, global_state_dict, global_lock):
     numeric_seed = int(world_id, 16)
-    w = World(seed=numeric_seed, num_altitude_spikes=(numeric_seed & 0xF/5)+2,
-              total_cells_desired=3000)
+    w = world_one.generate_world(seed=numeric_seed, total_cells_desired=3000)
     t = geography.topography_from_geography(geography.geography_from_everett_world(w))
     # for a in range(30):
     #     logger.debug(u"Waiting for {} seconds...".format(a))
