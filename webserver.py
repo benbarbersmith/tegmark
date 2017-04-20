357da00beae305138f1a4a399deb24fca6fd30f9
Benjamin Smith <ben+github@benjaminasmith.com>
2017-04-20 21:25:33 +0100

    Move serving duties for the frontend into the existing Flask webserver.

--- webserver.py
+++ webserver.py
@@ -1,4 +1,24 @@
-from api.web import app
+from flask import Flask
+from flask import render_template
+from flask.ext.cors import CORS
+from flask_assets import Environment
+
+from api.blueprints.worlds.views import blueprint_api
+
+app = Flask(__name__)
+
+app.secret_key = "BANANAS"  # TODO: generate later
+CORS(app)
+
+assets = Environment()
+assets.init_app(app)
+
+app.register_blueprint(blueprint_api, url_prefix='/api')
+
+@app.route('/')
+@app.route('/<id>')
+def hello(world_id=None):
+    return render_template('world.html', world_id=id)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=15000)
