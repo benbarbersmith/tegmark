128d221fc9b722ed39c32f35a7f7aac25d48749c
Sam Corcoran <samcorcoran@improbable.io>
2017-04-21 23:55:44 +0100

    Tegmark worldgen timer print statement for quick checking of performance

--- worldserver/generation.py
+++ worldserver/generation.py
@@ -1,10 +1,13 @@
 from everett.worldgenerators import world_three
 from everett.export import wheeler
-
+from timeit import default_timer as timer
 
 def add_world(world_id, global_state_dict, global_lock):
     numeric_seed = int(world_id, 16)
-    w = world_three.generate_world(seed=numeric_seed, total_cells_desired=10000)
+    start = timer()
+    w = world_three.generate_world(seed=numeric_seed, total_cells_desired=100)
+    end = timer()
+    print("Tegmark total worldgen took {0}".format(end-start))
     # for a in range(30):
     #     logger.debug(u"Waiting for {} seconds...".format(a))
     #     time.sleep(1)
